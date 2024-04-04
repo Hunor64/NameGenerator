4218ae2a121323e8fd614a5e17c4c2b0c5b2d9f9
hunor64 <nhunor06@gmail.com>
2024-04-04 10:23:48 +0200

    Done

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
 
 namespace Generator
 {
@@ -27,7 +28,57 @@ namespace Generator
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
+            bool letezik = false;
 
+            var Gombok = this.panel.Children.OfType<Button>();
+            foreach (var elem in Gombok)
+            {
+                Button btn = (Button)elem;
+                if (btn.Name == txtName.Text)
+                {
+                    letezik = true;
+                    MessageBox.Show("Ez az elem már létezik!");
+                    break;
+                }
+            }
+            var Textek = this.panel.Children.OfType<TextBlock>();
+            foreach (var elem in Textek)
+            {
+                TextBlock txt = (TextBlock)elem;
+                if (txt.Name == txtName.Text)
+                {
+                    letezik = true;
+                    MessageBox.Show("Ez az elem már létezik!");
+                    break;
+                }
+            }
+            if (rdbButton.IsChecked == true)
+            {
+
+                if (!letezik)
+                {
+                    Button gomb = new();
+                    gomb.Click += NameButtonClick;
+                    gomb.Content = txtName.Text;
+                    gomb.Name = txtName.Text;
+                    panel.Children.Add(gomb);
+                }
+            }
+            else if (rdbTextBlock.IsChecked == true)
+            {
+                if (!letezik)
+                {
+                    TextBlock blokk = new();
+                    blokk.Text = txtName.Text;
+                    blokk.Name = txtName.Text;
+                    panel.Children.Add(blokk);
+                }
+            }
+        }
+        public void NameButtonClick(object sender, RoutedEventArgs e)
+        {
+            Button btn = (Button)sender;
+            MessageBox.Show($"Szia {btn.Name} vagyok!");
         }
     }
 }
